4bf8ead938be9ed07786e13287c5427a8165b37f
RagaviRajasekaran <143269843+RagaviRajasekaran@users.noreply.github.com>
2023-09-14 23:21:31 +0530

    Update Movie.js

--- Task6/Movie.js
+++ Task6/Movie.js
@@ -10,10 +10,10 @@ class Movie {
   }
 }
 
-// Creating an instance of the Movie class
+
 const casinoRoyale = new Movie("Casino Royale", "Eon Productions", "PG-13");
 
-// Example array of Movie instances
+
 const movies = [
   new Movie("Movie1", "Studio1", "PG"),
   new Movie("Movie2", "Studio2", "PG-13"),
@@ -21,6 +21,6 @@ const movies = [
   new Movie("Movie4", "Studio4"), // Defaults to "PG" rating
 ];
 
-// Using the getPG method to filter PG-rated movies
+
 const pgMovies = Movie.getPG(movies);
 console.log(pgMovies); // Should print an array with Casino Royale and Movie1
